066dc274cfe1d77637b8f11ac16c7323d26479a5
Arseny Malkov <arsenymalkov@gmail.com>
2020-08-21 18:50:45 +1200

    Gradient ppm

--- tinyraycaster/tinyraycaster.cpp
+++ tinyraycaster/tinyraycaster.cpp
@@ -1,20 +1,47 @@
-// tinyraycaster.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <cstdint>
+#include <cassert>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+uint32_t pack_color(const uint8_t r, const uint8_t g, const uint8_t b, const uint8_t a = 255) {
+    return (a << 24) + (b << 16) + (g << 8) + r;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+void unpack_color(const uint32_t& color, uint8_t& r, uint8_t& g, uint8_t& b, uint8_t& a) {
+    r = (color >> 0) & 255;
+    g = (color >> 8) & 255;
+    b = (color >> 16) & 255;
+    a = (color >> 24) & 255;
+}
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+void drop_ppm_image(const std::string filename, const std::vector<uint32_t>& image, const size_t w, const size_t h) {
+    assert(image.size() == w * h);
+    std::ofstream ofs(filename, std::ios::binary);
+    ofs << "P6\n" << w << " " << h << "\n255\n";
+    for (size_t i = 0; i < h * w; ++i) {
+        uint8_t r, g, b, a;
+        unpack_color(image[i], r, g, b, a);
+        ofs << static_cast<char>(r) << static_cast<char>(g) << static_cast<char>(b);
+    }
+    ofs.close();
+}
+
+int main() {
+    const size_t win_w = 512;
+    const size_t win_h = 512;
+    std::vector<uint32_t> framebuffer(win_w * win_h, 255);
+
+    for (size_t j = 0; j < win_h; j++) {
+        for (size_t i = 0; i < win_w; i++) {
+            uint8_t r = 255 * j / float(win_h);
+            uint8_t g = 255 * i / float(win_w);
+            uint8_t b = 0;
+            framebuffer[i + j * win_w] = pack_color(r, g, b);
+        }
+    }
+
+    drop_ppm_image("./out.ppm", framebuffer, win_w, win_h);
+
+    return 0;
+}
